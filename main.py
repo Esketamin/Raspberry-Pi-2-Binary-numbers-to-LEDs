e0c8c4a297feb4d707b0cc286fc9d41bdc94d007
Stefan <Dinge_passieren@web.de>
2015-07-03 11:30:48 +0200

    Replaced UML HTML Version to overcome language mix with zipped version. Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from time import sleep
 from LED import LED
-from test import *
 from BinaryNumber import BinaryNumber
   
 led1 = LED(12,1)
@@ -8,25 +7,12 @@ led2 = LED(26,1)
 led3 = LED(27,0)
 led4 = LED(28,0)
 
-
-
-
 leds = [led1, led2, led3, led4]
-
-# binGen = BinaryNumber(10)
-# print(binGen.getBinaryStringRepresentation())
-# 
-# binGen.trivialNumberSet(10, leds)
-# binGen.decrement(leds)
-# print(binGen.getBinaryStringRepresentation())
 
 binary = BinaryNumber(4,10)
 print(binary._bits)
 print(binary.getBinaryStringRepresentation())
 for i in range (0,binary._bits):
     print(binary._numberArray[i])
-
-# for led in leds:
-#     print(led.getState())
 
 sleep(1)
